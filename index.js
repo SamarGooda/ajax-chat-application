b65a341ab9f38df1d00b21a7ad9d313d604aec97
samar <samoramahmoud22@gmail.com>
2020-04-21 19:35:39 +0200

    add 2 end point (get,post)

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const app = express();
 
 
 app.use(express.json());
+const messages = [];
 
 
 
 
 
 
+app.post('/message',(req,res) =>{
+   const { body } = req ;
+   messages.push(body);
+   res.status(204).end();
+});
 
 
-app.listen(5000,(req , res) => {
+app.get('/message',(req,res) => { 
+   res.json(messages);
+}); 
+
+
+app.listen(3000,() => {
     console.info('Server listening on port 5000');
     
 
