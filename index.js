a79439ebcc8529ab966bc08560aa3bf28dfccf5f
samar <samoramahmoud22@gmail.com>
2020-04-22 04:18:21 +0200

    main file completed

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
+
 const messages = [];
 
 
-
-
-
-
 app.post('/message',(req,res) =>{
+    // console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh1")
    const { body } = req ;
    messages.push(body);
    res.status(204).end();
@@ -19,13 +18,32 @@ app.post('/message',(req,res) =>{
 
 
 app.get('/message',(req,res) => { 
+    // console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh2")
    res.json(messages);
+}); 
+// #############################################################
+const subscribes ={}
+
+app.post('/long',(req,res) =>{
+   const { body } = req ;
+
+   Object.keys(subscribes).forEach((id) => {
+     subscribes[id].json(body);
+     delete subscribes[id];
+}); 
+   res.status(204).end();
+}); 
+
+
+app.get('/long',(req,res) => { 
+    const id = Math.ceil(Math.random() * 100000);
+    subscribes[id]=res;
 }); 
 
 
 app.listen(3000,() => {
-    console.info('Server listening on port 5000');
-    
+    // console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh3")
+    console.info('Server listening on port 3000');  
 
 });
 
